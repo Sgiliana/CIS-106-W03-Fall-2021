34189d89750f0316945834fdb7f19bce729231cb
Sgiliana <91332529+Sgiliana@users.noreply.github.com>
2021-09-26 18:16:22 -0500

    Add files via upload

---
+++ ps1p1.py
@@ -0,0 +1,13 @@
+#comment input
+n1 = float(input("Enter a number"))
+n2 = float(input("Enter another number"))
+
+#process phase 
+d = n1 - n2
+s = n1 + n2 
+p = n1 * n2
+
+# output 
+print ("The sum is ", s)
+print ("The difference ", d)
+print ("The product is ", p)
