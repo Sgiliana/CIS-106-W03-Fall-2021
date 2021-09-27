2db076d203440996a81e65788212a0a0cf9f4ef7
Sgiliana <91332529+Sgiliana@users.noreply.github.com>
2021-09-26 19:33:47 -0500

    Add files via upload

---
+++ ps1p2.py
@@ -0,0 +1,11 @@
+#comment input
+l1 = (input("Enter last name "))
+s1 = float(input("Enter a test score "))
+s2 = float(input("Enter another test score "))
+
+#process phase 
+a1 = (s1 + s2) / 2 
+
+#output
+print ("The average score is", a1)
+print ("For ", l1)
