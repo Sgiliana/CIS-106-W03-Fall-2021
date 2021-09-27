aa208839cf97d355d873a43b6a02a54722f5a239
Sgiliana <91332529+Sgiliana@users.noreply.github.com>
2021-09-26 20:31:50 -0500

    Add files via upload

---
+++ ps1p5.py
@@ -0,0 +1,12 @@
+#input 
+a = float(input("Enter the amount of item: $"))
+
+#process phase 
+a = a
+t = 0.07
+t1 = a + (a * t)
+
+#output 
+print ("The amount of the item was $", a)
+print ("The tax for the item was 7%")
+print ("The total amount for the item is $", t1)
