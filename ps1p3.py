8f7d35335a28c37010327ad96f440c3e4ab23887
Sgiliana <91332529+Sgiliana@users.noreply.github.com>
2021-09-26 19:56:51 -0500

    Add files via upload

---
+++ ps1p3.py
@@ -0,0 +1,9 @@
+#comment input
+g1 = float(input("Enter gallons used "))
+m1 = float(input("Enter miles travelled "))
+
+#process phase
+g2 = (m1 / g1)
+
+# output
+print ("The miles per gallon is ", g2)
